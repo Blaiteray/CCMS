8af1ff09df93f9c096cd5bd042ffe08d1d6971c1
Blaiteray <soumitra.phi@gmail.com>
2023-04-11 22:38:15 +0600

    adding files

---
+++ UI/category_selection.py
@@ -0,0 +1,22 @@
+import kivy
+kivy.require('2.1.0')
+
+from kivy.graphics import Color, Rectangle
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+# from kivy.uix.screenmanager import Screen, ScreenManager
+from modified_widget import *
+
+
+
+class Category_Selection_Widget(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        #write your code from here
