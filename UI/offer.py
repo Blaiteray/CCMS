6d0241c88cd9628085a9d0d5e4e019c1bab5245b
Blaiteray <soumitra.phi@gmail.com>
2023-04-11 23:48:41 +0600

    finishing offer

--- UI/offer.py
+++ UI/offer.py
@@ -14,6 +14,19 @@ from kivy.uix.textinput import TextInput
 # from kivy.uix.screenmanager import Screen, ScreenManager
 from modified_widget import *
 
+offer_list = [
+    ["ix12b5", 60, 4.99, "12-01-2023"],
+    ["ix12b5", 160, 4.99, "12-01-2023"],
+    ["ix12b5", 1260, 4.99, "12-01-2023"],
+    ["ix12b5", 60, 4.99, "12-01-2023"],
+    ["ix12b5", 60, 40.99, "12-01-2023"],
+    ["ix12b5", 60, 99.99, "12-01-2023"],
+    ["ix12b5", 60, 4.99, "12-01-2023"],
+    ["ix12b5", 60, 4.99, "12-01-2023"],
+    ["ix12b5", 60, 4.99, "12-01-2023"],
+    ["ix12b5", 60, 4.99, "12-01-2023"],
+    ["ix12b4", 60, 4.99, "12-01-2023"],
+]
 
 
 class Offer_Widget(FloatLayout):
@@ -52,28 +65,46 @@ class Offer_Widget(FloatLayout):
             Color(41/255, 50/255, 70/255, 0.9)
             Rectangle(pos=(60, 80), size=(680, 390))
 
-        self.note_list = ["bobbo", "mamama", "fes", "feasfes", "hello woerld", "mama", "banana", "meau", "esa"]
-        self.note_list_container = ScrollView(
-            pos=(80, 115), 
-            size=(640, 320), 
-            size_hint=(None, None),
-            scroll_wheel_distance = 100)
-        self.note_button_container = BoxLayout(orientation='vertical',size_hint_y=None, height=len(self.note_list)*40)
-        
+        self.offer_list = []
+        for item in offer_list:
+            minutes = " "*(5-len(str(item[1])))+str(item[1])
+            price = str(item[2])+" "*(6-len(str(item[2])))
+            self.offer_list.append(f"  {item[0]}             {minutes} Minutes        $ {price}       {item[3]}  ")
 
-        for note in self.note_list:
-            self.note_containing_buttion = HoverButton((41/255, 50/255, 70/255, 0.7), (41/255, 50/255, 70/255, 0.9), 
-                    text=note, 
-                    background_color=(41/255, 50/255, 70/255, 0.9),
-                    font_name="assets/static/Nunito-Regular",
+        self.dummy_button = HoverButton((41/255, 50/255, 70/255, 1), (41/255, 50/255, 70/255, 1), 
+                    text=f"  Offer ID                Minutes          Price         Expire Date  ", 
+                    background_color=(41/255, 50/255, 70/255, 1),
+                    pos=(70, 395),
+                    size_hint_x=None,
+                    width=660,
+                    font_name="assets/Inconsolata-Bold",
                     font_size='16sp',
                     size_hint_y=None,
                     border=(-20, 16, 20, 16),
                     height=40)
-            self.note_button_container.add_widget(self.note_containing_buttion)
-        self.note_list_container.add_widget(self.note_button_container)
+        
+        self.offer_list_container = ScrollView(
+            pos=(80, 115), 
+            size=(640, 280), 
+            size_hint=(None, None),
+            scroll_wheel_distance = 100)
+        self.offer_button_container = BoxLayout(orientation='vertical',size_hint_y=None, height=len(self.offer_list)*40)
+        
+
+        for offer in self.offer_list:
+            self.offer_containing_buttion = HoverButton((41/255, 50/255, 70/255, 0.7), (41/255, 50/255, 70/255, 0.9), 
+                    text=offer, 
+                    background_color=(41/255, 50/255, 70/255, 0.9),
+                    font_name="assets/Inconsolata-Bold",
+                    font_size='16sp',
+                    size_hint_y=None,
+                    border=(-20, 16, 20, 16),
+                    height=40)
+            self.offer_button_container.add_widget(self.offer_containing_buttion)
+        self.offer_list_container.add_widget(self.offer_button_container)
 
         self.add_widget(self.main_label)
         self.add_widget(self.cancel_button)
         self.add_widget(self.history_button)
-        self.add_widget(self.note_list_container)
+        self.add_widget(self.dummy_button)
+        self.add_widget(self.offer_list_container)
